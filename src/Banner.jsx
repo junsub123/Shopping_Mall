31a9ca1de657d1c9e61b3a29d066cf15c9d1e8ba
subsub <89231026+junsub123@users.noreply.github.com>
2023-06-02 17:30:34 +0900

    commit
    commit

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -10,13 +10,26 @@ export default function Banner() {
     return (
         <div>
             <Swiper
-                modules={[Pagination, Autoplay]}
+                modules={[Navigation,Pagination, Autoplay]}
                 pagination={{ clickable: true }}
                 navigation={true}
                 loop={true} // 루프 슬라이드
                 autoplay={{ delay: 1000 }} // 자동 슬라이드
                 spaceBetween={0} // 슬라이드간의 간격
                 slidesPerView={1} // 한 번에 보여지는 슬라이드 개수
+                breakpoints={{
+                    0: {
+                        slidesOffsetBefore: 0,
+                        slidesPerView: 1,
+                        spaceBetween: 8,
+                        centeredSlides: false,
+                    },
+                    500: {
+                        slidesOffsetBefore: 16,
+                        slidesPerView: 2,
+                        spaceBetween: 8,
+                        centeredSlides: false,
+                    }}}
             >
                 <SwiperSlide >
                     <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fshop1.phinf.naver.net%2F20220725_132%2F1658712269413vgdsd_PNG%2F59848112123002374_1751877375.PNG&type=sc960_832" alt="" />
